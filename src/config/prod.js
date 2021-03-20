27e63fc129603385464bba00444e0d995fa94cae
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-03-19 20:37:12 -0700

    temporary fix

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,11 +1,23 @@
+// module.exports = {
+//     firebaseApiKey: process.env.FIREBASE_API_KEY,
+//     firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+//     firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     firebaseAppId: process.env.FIREBASE_APP_ID,
+//     firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//     firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
+//     adminId: process.env.ADMIN_ID,
+// };
+
 module.exports = {
-    firebaseApiKey: process.env.FIREBASE_API_KEY,
-    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    firebaseAppId: process.env.FIREBASE_APP_ID,
-    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
-    adminId: process.env.ADMIN_ID,
+    firebaseApiKey: 'AIzaSyBzKc8dYF4PxhxMxJnAsc92SEK55fH0s9c',
+    firebaseAuthDomain: 'assettocorsagarage.firebaseapp.com',
+    firebaseProjectId: 'assettocorsagarage',
+    firebaseStorageBucket: 'assettocorsagarage.appspot.com',
+    firebaseMessagingSenderId: '220523058474',
+    firebaseAppId: '1:220523058474:web:c21aef7bfe3e5139263a53',
+    firebaseMeasurementId: 'G-8Q4JQ8FJNX',
+    firebaseDatabaseURL: 'https://assettocorsagarage-default-rtdb.firebaseio.com/',
+    adminId: 'jXA3EsCxBqa6KFRiQ87zgJLlo6c2',
 };
